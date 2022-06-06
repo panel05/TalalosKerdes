c304734e7fcdfbb9159a71c34799ea7283eb5b24
panel05 <pantlbalazs@gmail.com>
2022-06-06 15:36:39 +0200

    javitott console

--- src/melyik/melyikLada.java
+++ src/melyik/melyikLada.java
@@ -2,11 +2,12 @@ package melyik;
 import java.util.*;
 
 public class melyikLada {
-    private String[] szin = new String[]{"arany", "ezüst", "bronz"};
+    private String[] szin = new String[]{"arany", "ezust", "bronz"};
     private String[] LadaVes = new String[]{"Nálam van a kincs", "Nem nálam van a kincs", "Arany hazudik"};
     private String feladat = "3 láda közül az egyik rejti a kincset. Mindegyiken van egy felirat (ami nem változtatható, mert belevésték a ládába), de csak az egyik láda állítása igaz!";
     private static Scanner sc = new Scanner(System.in);
     private String valasz;
+    private String helytelen = "Nem ebben volt";
     
     public void Kerdes(){
         System.out.println(feladat);
@@ -20,10 +21,15 @@ public class melyikLada {
         felhasznaloValasz();
     }
     public void felhasznaloValasz(){
-        String helyes = "ezüst";
+        String helyes = "ezust";
         for (int i = 0; i < szin.length; i++) {
-            if (valasz.equals(szin[1])) {
+            if (valasz.equals(szin[i].equals("ezust"))) {
                 System.out.println("Ebben van a kincs.");
+                i++;
+            }else{
+                i++;
+                System.out.println(helytelen);
+                
             }
             
         
